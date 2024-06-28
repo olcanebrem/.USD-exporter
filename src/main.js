d9b2051df1dfc994a52e6d90827f817b32347b9b
Olcan EBREM <olcanebrem@gmail.com>
2024-06-28 23:21:38 +0300

    2

--- src/main.js
+++ src/main.js
@@ -1,54 +1,20 @@
-import { Client, Databases, ID } from "node-appwrite";
+import { Client, Databases, ID } from "appwrite";
 
 const client = new Client()
-    .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
-    .setProject('667eeaf9002912d4f3ab'); // Your project ID
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('667eeaf9002912d4f3ab');
 
 const databases = new Databases(client);
 
-// Wrap the database operation in an async function
-async function createDocument() {
-    try {
-        const result = await databases.createDocument(
-            '667efb7e00313876acb2', // databaseId
-            '667efbad0031c4393190', // collectionId
-            ID.unique(), // documentId
-            {
-                "nickName": "yessir3000porn0",
-                "age": "12",
-                "note": [4.4554],
-            }, // data
-            ["*"] // permissions (optional)
-        );
+const promise = databases.createDocument(
+    '667efb7e00313876acb2',
+    '667efbad0031c4393190',
+    ID.unique(),
+    { "title": "Hamlet" }
+);
 
-        console.log(result);
-    } catch (error) {
-        console.error("Error creating document:", error);
-    }
-}
-      
-createDocument();
-
-// This is your Appwrite function
-// It's executed each time we get a request
-export default async ({ req, res, log, error }) => {
-    // You can log messages to the console
-    log('Hello, Logs!');
-    // If something goes wrong, log an error
-    error('Hello, Errors!');
-
-    // The `req` object contains the request data
-    if (req.method === 'GET') {
-        // Send a response with the res object helpers
-        // `res.send()` dispatches a string back to the client
-        return res.send('Hello, World!');
-    }
-
-    // `res.json()` is a handy helper for sending JSON
-    return res.json({
-        motto: 'Build like a team of hundreds_',
-        learn: 'https://appwrite.io/docs',
-        connect: 'https://appwrite.io/discord',
-        getInspired: 'https://builtwith.appwrite.io',
-    });
-};
+promise.then(function (response) {
+    console.log(response);
+}, function (error) {
+    console.log(error);
+});
