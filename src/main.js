e093a1198ebcea7ea3ac9eed4bc1932c293c88f6
Olcan EBREM <olcanebrem@gmail.com>
2024-06-28 21:19:19 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
-
-import { Client, Databases } from "appwrite";
+import { Client, Databases } from "node-appwrite";
 
 const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
