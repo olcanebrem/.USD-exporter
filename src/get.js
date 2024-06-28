dafdf9a7f5c2d8abdb6eed1715eb900ada38424c
Olcan EBREM <olcanebrem@gmail.com>
2024-06-29 02:39:38 +0300

    app

--- src/get.js
+++ src/get.js
@@ -1,26 +1,28 @@
-import * as Appwrite from 'appwrite';
 
-// Appwrite bağlantısı oluşturma
-const client = new Appwrite();
-client
-    .setEndpoint('https://cloud.appwrite.io') // Appwrite endpoint'i
-    .setProject('667eeaf9002912d4f3ab') // Appwrite proje ID'si
-    .setKey('YOUR_API_KEY'); // API anahtarınız
+import { Client, Databases, ID } from 'appwrite';
 
-// Belgeyi alma işlevi
-async function fetchDocument(documentId) {
-    try {
-        const response = await client.database.getDocument(
-            '667efb7e00313876acb2', // Veritabanı ID'si
-            '667efbad0031c4393190', // Koleksiyon ID'si
-            documentId // Belge ID'si
-        );
+// Appwrite Client oluşturma
+const client = new Client()
+    .setEndpoint('https://cloud.appwrite.io/v1') // Appwrite endpoint'i
+    .setProject('667eeaf9002912d4f3ab'); // Appwrite proje ID'si, Appwrite panelinizden alınmalıdır
 
-        console.log('Fetched Document:', response);
-    } catch (error) {
-        console.error('Error fetching document:', error);
+// Databases nesnesini oluşturma
+const databases = new Databases(client);
+
+// Belge oluşturma işlemi
+const promise = databases.createDocument(
+    '667efb7e00313876acb2', // Veritabanı ID'si, Appwrite panelinizden alınmalıdır
+    '667efbad0031c4393190', // Koleksiyon ID'si, Appwrite panelinizden alınmalıdır
+    ID.unique(), // Benzersiz belge ID'si oluşturma
+    { "title": "Hamlet" } // Belge verileri
+);
+
+// Belge oluşturma işleminin sonucunu işleme
+promise.then(
+    function (response) {
+        console.log('Document created:', response);
+    },
+    function (error) {
+        console.error('Error creating document:', error);
     }
-}
-
-// Belgeyi alma işlevini çağırın
-fetchDocument('documentId');
+);
