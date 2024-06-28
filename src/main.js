21edd73bbf88afa0380fa9e1443dde3591148812
Olcan EBREM <olcanebrem@gmail.com>
2024-06-28 21:23:07 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { Client, Databases } from "node-appwrite";
+import { Client, Databases, ID } from "node-appwrite";
 
 const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
@@ -6,49 +6,49 @@ const client = new Client()
 
 const databases = new Databases(client);
 
-const result = await databases.createDocument(
-    '667efb7e00313876acb2', // databaseId
-    '667efbad0031c4393190', // collectionId
-    ID.unique(), // documentId
-    {
-      "nickName":"yessir3000porn0",
-      "age":"12",
-      "note":[4.4554],
-    }, // data
-    ["read("any")"] // permissions (optional)
-);
+// Wrap the database operation in an async function
+async function createDocument() {
+    try {
+        const result = await databases.createDocument(
+            '667efb7e00313876acb2', // databaseId
+            '667efbad0031c4393190', // collectionId
+            ID.unique(), // documentId
+            {
+                "nickName": "yessir3000porn0",
+                "age": "12",
+                "note": [4.4554],
+            }, // data
+            ["*"] // permissions (optional)
+        );
 
-console.log(response);
+        console.log(result);
+    } catch (error) {
+        console.error("Error creating document:", error);
+    }
+}
+
+createDocument();
 
 // This is your Appwrite function
 // It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
-  // Why not try the Appwrite SDK?
-  //
-  // const client = new Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //    .setKey(process.env.APPWRITE_API_KEY);
+    // You can log messages to the console
+    log('Hello, Logs!');
+    // If something goes wrong, log an error
+    error('Hello, Errors!');
 
-  // You can log messages to the console
-  log('Hello, Logs!');
-  // If something goes wrong, log an error
-  error('Hello, Errors!');
+    // The `req` object contains the request data
+    if (req.method === 'GET') {
+        // Send a response with the res object helpers
+        // `res.send()` dispatches a string back to the client
+        return res.send('Hello, World!');
+    }
 
-  // The `req` object contains the request data
-  if (req.method === 'GET') {
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
-    return res.send('Hello, World!');
-  }
-
-  // `res.json()` is a handy helper for sending JSON
-  return res.json({
-    motto: 'Build like a team of hundreds_',
-    learn: 'https://appwrite.io/docs',
-    connect: 'https://appwrite.io/discord',
-    getInspired: 'https://builtwith.appwrite.io',
-  });
+    // `res.json()` is a handy helper for sending JSON
+    return res.json({
+        motto: 'Build like a team of hundreds_',
+        learn: 'https://appwrite.io/docs',
+        connect: 'https://appwrite.io/discord',
+        getInspired: 'https://builtwith.appwrite.io',
+    });
 };
-
-
