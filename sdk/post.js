679614be21d0a9f5dbb48c3afe86ff2830a8b8e7
Olcan EBREM <olcanebrem@gmail.com>
2024-07-02 01:40:49 +0300

    postjs

--- sdk/post.js
+++ sdk/post.js
@@ -1,4 +1,4 @@
-import { Client, Databases ,ID } from 'node-appwrite';
+import { Client, Databases } from 'node-appwrite';
 
 const PROJECT_ID = process.env.PROJECT_ID;
 const DB_ID = process.env.DB_ID;
@@ -11,27 +11,16 @@ export default async ({ req, res, log, error }) => {
 
   const db = new Databases(client);
 
-  if (req.method == 'POST') {
-    
-    const { data, read, write } = req.body; // POST isteği ile gönderilen veri
-    data = {
-      "age": 1,
-      "note": [],
-      "title": "works",
-      "numb": null,
-      "$id": "667f221500115624a682",
-      "$tenant": "159567",
-      "$createdAt": "2024-06-28T20:50:29.678+00:00",
-      "$updatedAt": "2024-06-28T20:59:54.102+00:00",
-      "$permissions": [],
-      "$databaseId": "667efb7e00313876acb2",
-      "$collectionId": "667efbad0031c4393190"
-    };
-    const response = await db.createDocument(DB_ID, COLLECTION_ID_PROFILES, ID.unique(), data, read, write);
-  
-    return res.json(response);
-    
+  if (req.method === 'POST') {
+    try {
+      const { data, read, write } = req.body; // POST isteği ile gönderilen veri
+      const response = await db.createDocument(DB_ID, COLLECTION_ID_PROFILES, 'unique()', data, read, write);
+      return res.json(response);
+    } catch (err) {
+      console.error("Error creating document:", err);
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+  } else {
+    return res.status(405).json({ error: "Method Not Allowed" });
   }
-
-  return res.send("Method Not Allowed");
 };
