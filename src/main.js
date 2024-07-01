0efc892ca837080b5c98e8bf875614ad8a3e15d4
Olcan EBREM <olcanebrem@gmail.com>
2024-07-01 23:36:36 +0300

    src main

--- src/main.js
+++ src/main.js
@@ -11,16 +11,16 @@ export default async ({ req, res, log, error }) => {
     .setKey(process.env.APPWRITE_API_KEY);
 
   // The `req` object contains the request data
+  // You can log messages to the console
+  log('Hello, Logs!');
+  
+  // If something goes wrong, log an error
+  error('Hello, Errors!');
+  
   if (req.method === 'GET') {
     console.log(jsonResponse);
     return res.send('Hello, World!');
   }
-  // You can log messages to the console
-  log('Hello, Logs!');
-
-  // If something goes wrong, log an error
-  error('Hello, Errors!');
-
   // `res.json()` is a handy helper for sending JSON
   return res.json({
     motto: 'Build like a team of hundreds_',
