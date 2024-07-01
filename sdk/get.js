472c4d5ff2eaf06001725c744aa02c6e20fbbf23
Olcan EBREM <olcanebrem@gmail.com>
2024-07-01 17:44:22 +0300

    sdk test

--- sdk/get.js
+++ sdk/get.js
@@ -1,15 +1,15 @@
 import { Databases } from "appwrite";
 
-const PROJECT_ID = "667eeaf9002912d4f3ab"
-const DB_ID = "667efb7e00313876acb2"
-const COLEECTION_ID_PROFILES = "667efbad0031c4393190"
+const PROJECT_ID = process.env.PROJECT_ID
+const DB_ID = process.env.DB_ID
+const COLEECTION_ID_PROFILES = process.env.COLEECTION_ID_PROFILES
 
 export default async ({req, res, log, error}) => {
 
         const client = new client()
         
         client
-                .setEndpoint("667eeaf9002912d4f3ab")
+                .setEndpoint("https://cloud.appwrite.io/v1")
                 .setProject(PROJECT_ID)
 
         const db = new Databases(client)
