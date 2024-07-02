c36a0d6281872a73cf03e2558009112164d21c4a
Olcan EBREM <olcanebrem@gmail.com>
2024-07-02 22:30:34 +0300

    postjs

--- sdk/post2.js
+++ sdk/post2.js
@@ -1,12 +1,12 @@
 import { Client, Databases, ID } from 'appwrite';
-export default async ({ req, res, log, error }) => {
+
 // Initialize the Appwrite client
 const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1') // Your Appwrite endpoint
     .setProject('667eeaf9002912d4f3ab'); // Your project ID
 
+export default async ({ req, res }) => {
     if (req.method === 'POST') {
-       
         const databases = new Databases(client);
 
         try {
@@ -19,12 +19,12 @@ const client = new Client()
                 }
             );
             console.log(response); // Output the response
+            return res.status(200).send('Document created successfully');
         } catch (error) {
             console.error('Error creating document:', error);
+            return res.status(500).send('Error creating document');
         }
-        console.log;
-        return res.send('Hello, World!');
-      }
-
-}
-createDocument();
+    } else {
+        return res.status(405).send('Method Not Allowed');
+    }
+};
