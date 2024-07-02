08e8dcce6f9087eaf734d47a9bf7a30be2c42793
Olcan EBREM <olcanebrem@gmail.com>
2024-07-02 17:24:33 +0300

    postjs

--- sdk/post2.cjs
+++ sdk/post2.cjs
@@ -3,7 +3,8 @@ const sdk = require('node-appwrite');
 const PROJECT_ID = process.env.PROJECT_ID || 'your_project_id';
 const DB_ID = process.env.DB_ID || 'your_db_id';
 const COLLECTION_ID_PROFILES = process.env.COLLECTION_ID_PROFILES || 'your_collection_id';
-
+// Call the async function
+createDocument();
 const createDocument = async () => {
     const client = new sdk.Client()
         .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
@@ -26,5 +27,4 @@ const createDocument = async () => {
     }
 };
 
-// Call the async function
-createDocument();
+
