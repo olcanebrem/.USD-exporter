9a117e64aa2e42574c2496fcdc2fda1d5c8a9b4b
Olcan EBREM <olcanebrem@gmail.com>
2024-07-02 21:36:35 +0300

    postjs

--- sdk/post2.js
+++ sdk/post2.js
@@ -14,7 +14,7 @@ export default async ({ req, res, log, error }) => {
 
         const promise = new Databases(client);
 
-        try {
+        
             const response = await promise.createDocument(
                 DB_ID, // databaseId
                 COLLECTION_ID_PROFILES, // collectionId
@@ -22,18 +22,12 @@ export default async ({ req, res, log, error }) => {
                 { "title": "hello world" }, // data
                 [] // permissions (optional)
             );
-            console.log(response);
-            return res.json({ success: true, data: response });
-        } catch (err) {
-            console.error(err);
-            return res.json({ success: false, error: err.message });
-        }
-    }
-
-    if (req.method === 'POST') {
-        await createDocument();
-    } else {
-        return res.json({ success: false, error: 'Method Not Allowed' });
+            
+            if (req.method === 'POST') {
+                await createDocument();
+            } else {
+                return res.json({ success: false, error: 'Method Not Allowed' });
+            }
     }
     createDocument();
 }
