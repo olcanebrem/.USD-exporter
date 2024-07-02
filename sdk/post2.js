d5be8fdefbd0ae88b3965536bbd8e4c4d243a9dd
Olcan EBREM <olcanebrem@gmail.com>
2024-07-02 19:55:06 +0300

    postjs

--- sdk/post2.js
+++ sdk/post2.js
@@ -1,33 +1,34 @@
-const sdk = require("node-appwrite");
+import { client, Databases, ID } from "appwrite";
+
 const PROJECT_ID = process.env.PROJECT_ID
 const DB_ID = process.env.DB_ID
 const COLLECTION_ID_PROFILES = process.env.COLLECTION_ID_PROFILES
 
 
-async function createDocument() {
-       
-        const client = new sdk.Client()
+    async function addTask(e){
+        e.preventDefault()
+
+        const taskBody = e.target.body.value
+        if (taskBody == ''){
+        alert('Form cannot be empty!')
+        return
+        }
+        client
             .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
             .setProject(PROJECT_ID); // Your project ID
     
-        const Databases = new sdk.Databases(client);
+        const db = new Databases(client);
     
-        try {
-            const response = await Databases.createDocument(
-                DB_ID, // databaseId
-                COLLECTION_ID_PROFILES, // collectionId
-                sdk.ID.unique(), // documentId
-                {}, // data
-                [] // permissions (optional)
+        
+        const response = await Databases.createDocument(
+            DB_ID, // databaseId
+            COLLECTION_ID_PROFILES, // collectionId
+            ID.unique(), // documentId
+            {"body":0}, // data
+            [] // permissions (optional)
             );
-    
-            console.log('Document created:', result);
-        } catch (error) {
-            console.error('Error creating document:', error);
-        }
-    };
-    
 
-
+    }
+    
 // Call the async function
 createDocument();
