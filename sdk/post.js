563787f425f8f6a69eaa9adb910f4e2d5c711ead
Olcan EBREM <olcanebrem@gmail.com>
2024-07-02 15:26:32 +0300

    postjs

--- sdk/post.js
+++ sdk/post.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { Client, Databases, ID } = require('node-appwrite');
+import { Client, Databases, ID } from ('node-appwrite');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
