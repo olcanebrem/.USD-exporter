1474d372ba36411ad9f1dc234187b000339ed7c8
Olcan EBREM <olcanebrem@gmail.com>
2024-07-02 22:28:50 +0300

    postjs

--- sdk/post2.js
+++ sdk/post2.js
@@ -5,21 +5,26 @@ const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1') // Your Appwrite endpoint
     .setProject('667eeaf9002912d4f3ab'); // Your project ID
 
-// Initialize the Databases service
-const databases = new Databases(client);
+    if (req.method === 'POST') {
+       
+        const databases = new Databases(client);
 
-    try {
-        const response = await databases.createDocument(
-            '667efb7e00313876acb2', // Database ID
-            '667efbad0031c4393190', // Collection ID
-            ID.unique(), // Document ID (Appwrite will generate a unique ID)
-            {
-                age: 1,
-            }
-        );
-        console.log(response); // Output the response
-    } catch (error) {
-        console.error('Error creating document:', error);
-    }
+        try {
+            const response = await databases.createDocument(
+                '667efb7e00313876acb2', // Database ID
+                '667efbad0031c4393190', // Collection ID
+                ID.unique(), // Document ID (Appwrite will generate a unique ID)
+                {
+                    age: 1,
+                }
+            );
+            console.log(response); // Output the response
+        } catch (error) {
+            console.error('Error creating document:', error);
+        }
+        console.log;
+        return res.send('Hello, World!');
+      }
+
 }
 createDocument();
