7ff6ed10f9941d62e2543e8853ab5aebd3a0474e
Olcan EBREM <olcanebrem@gmail.com>
2024-07-02 19:38:14 +0300

    postjs

--- sdk/post2.js
+++ sdk/post2.js
@@ -1,17 +1,11 @@
 const sdk = require("node-appwrite");
-const PROJECT_ID = process.env.PROJECT_ID || 'your_project_id';
-const DB_ID = process.env.DB_ID || 'your_db_id';
-const COLLECTION_ID_PROFILES = process.env.COLLECTION_ID_PROFILES || 'your_collection_id';
+const PROJECT_ID = process.env.PROJECT_ID
+const DB_ID = process.env.DB_ID
+const COLLECTION_ID_PROFILES = process.env.COLLECTION_ID_PROFILES
 
 
-async function createDocument(e) {
-        e.preventDefault()
-        const taskBody = e.target.body.value
-
-        if(taskBody == ""){
-            alert("form cannot be emty")
-            return
-        }
+async function createDocument() {
+       
         const client = new sdk.Client()
             .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
             .setProject(PROJECT_ID); // Your project ID
