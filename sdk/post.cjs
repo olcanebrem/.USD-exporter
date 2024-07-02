b54df4cd4ed453299d27d14ea5c136e78c7b3526
Olcan EBREM <olcanebrem@gmail.com>
2024-07-02 15:51:59 +0300

    postjs

--- sdk/post.cjs
+++ sdk/post.cjs
@@ -7,12 +7,12 @@ const COLLECTION_ID_PROFILES = process.env.COLLECTION_ID_PROFILES;
 const client = new sdk.Client()
     .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
     .setProject(PROJECT_ID) // Your project ID
-    .setSession(''); // The user session to authenticate with
+
 
 const databases = new sdk.Databases(client);
 
 const result = await databases.listDocuments(
     DB_ID, // databaseId
     COLLECTION_ID_PROFILES, // collectionId
-    [] // queries (optional)
+    false // enabled (optional)
 );
