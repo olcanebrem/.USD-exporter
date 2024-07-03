4a1925fd4d4b2da6510c0ac84ef705e3dca89233
Olcan EBREM <olcanebrem@gmail.com>
2024-07-03 22:15:57 +0300

    cors

--- sdk/get.js
+++ sdk/get.js
@@ -30,17 +30,11 @@ export default async ({ req, res, log, error }) => {
               res.status(500).json({ error: 'Internal Server Error' });
           }
         });
-        if (req.method == 'GET') {
+        if (req.method == 'uGET') {
           const response = await db.listDocuments(DB_ID, COLLECTION_ID_PROFILES);
-          // Middleware to set CORS headers
-          app.use((req, res, next) => {
-            res.header('Access-Control-Allow-Origin', 'https://olcanebrem.com');
-            res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-            res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-            next();
+          
             return res.json(response.documents);
-        });
+        };
         }
 
   return res.send('error');
-};
