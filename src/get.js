80e1120bae04369d0930c5f0458924ebb84cbbfa
Olcan EBREM <olcanebrem@gmail.com>
2024-07-04 00:01:18 +0300

    cors

--- src/get.js
+++ src/get.js
@@ -1,13 +1,13 @@
-async function createDocument() {
+async function fetchDocuments() {
   const url =
     'https://cloud.appwrite.io/v1/databases/667efb7e00313876acb2/collections/667efbad0031c4393190/documents';
 
- 
   const response = await fetch(url, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
       'X-Appwrite-Project': '667eeaf9002912d4f3ab', // Your project ID
+      'X-Appwrite-Key': 'your_api_key', // Your API key
     },
   });
 
@@ -15,8 +15,8 @@ async function createDocument() {
     const jsonResponse = await response.json();
     console.log(jsonResponse);
   } else {
-    console.error('Error creating document:', response.statusText);
+    console.error('Error fetching documents:', response.statusText);
   }
 }
 
-createDocument();
+fetchDocuments();
