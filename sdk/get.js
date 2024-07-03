5edf64ede074d1073fe0ac0a28f6ab0b52ebc36f
Olcan EBREM <olcanebrem@gmail.com>
2024-07-03 23:19:42 +0300

    cors

--- sdk/get.js
+++ sdk/get.js
@@ -1,41 +1,45 @@
 import { Client, Databases } from 'node-appwrite';
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
 
-dotenv.config();
+
 
 const app = express();
 const PROJECT_ID = process.env.PROJECT_ID;
 const DB_ID = process.env.DB_ID;
 const COLLECTION_ID_PROFILES = process.env.COLLECTION_ID_PROFILES;
 
-const client = new Client();
-client
-    .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject(PROJECT_ID);
+export default async ({ req, res, log, error }) => {
 
-const db = new Databases(client);
+        const client = new Client();
 
-// CORS ayarları
+        client
+            .setEndpoint('https://cloud.appwrite.io/v1')
+            .setProject(PROJECT_ID);
+
+        const db = new Databases(client);
+
+        // CORS ayarları
 app.use(cors({
   origin: 'https://olcanebrem.com',
   methods: 'GET, POST, PUT, DELETE, OPTIONS',
   allowedHeaders: 'Content-Type, Authorization'
 }));
 
-// GET endpoint
-app.get('/api/documents', async (req, res) => {
-    try {
-        const response = await db.listDocuments(DB_ID, COLLECTION_ID_PROFILES);
-        res.json(response.documents);
-    } catch (error) {
-        console.error('Error fetching documents:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
+        
+        app.get('/api/documents', async (req, res) => {
+          try {
+              const response = await db.listDocuments(DB_ID, COLLECTION_ID_PROFILES);
+              res.json(response.documents);
+          } catch (error) {
+              console.error('Error fetching documents:', error);
+              res.status(500).json({ error: 'Internal Server Error' });
+          }
+        });
+        if (req.method == 'GET') {
+          const response = await db.listDocuments(DB_ID, COLLECTION_ID_PROFILES);
+          return res.json(response.documents);
+        }
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+  return res.send('error');
+};
