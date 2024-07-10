8feac715d5566ebf8b22a4f7a471c754a6d66be6
Olcan EBREM <olcanebrem@gmail.com>
2024-07-10 12:45:54 +0300

    accountnew

--- sdk/account.js
+++ sdk/account.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
                 ID.unique(), // userId
                 email,
                 password,
-                name || '' // Optional: If name is not provided, pass an empty string
+                name
             );
             console.log(result); // Output the response
             return res.status(200).json({ message: 'Account created successfully', result });
