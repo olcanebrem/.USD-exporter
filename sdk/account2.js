bc15bd1629f99b023735a4a8a232bc70cb3acf15
Olcan EBREM <olcanebrem@gmail.com>
2024-07-10 18:04:37 +0300

    acc2

--- sdk/account2.js
+++ sdk/account2.js
@@ -1,33 +1,25 @@
-const PROJECT_ID = process.env.PROJECT_ID;
-const userId = 'aUSERdfdIDa';
-const email = 'edlcanedfdfbrem@gmail.com';
-const password = 'youfdr_password';
-const name = 'Johfn Doe';
+const express = require('express');
+const { Client, Users } = require('appwrite');
 
-const requestData = {
-  userId,
-  email,
-  password,
-  name
-};
+const app = express();
 
-const requestOptions = {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    'X-Appwrite-Response-Format': '1.5.0',
-    'X-Appwrite-Project': "667eeaf9002912d4f3ab" // Replace with your project ID
-  },
-  body: JSON.stringify(requestData)
-};
+app.use(express.json());
 
-fetch('https://cloud.appwrite.io/v1/account', requestOptions)
-  .then(response => response.json())
-  .then(data => {
-    console.log('User created:', data);
-    // Handle the response data as needed
-  })
-  .catch(error => {
-    console.error('Error creating user:', error);
-    // Handle errors
-  });
+app.post('/createUser', async (req, res) => {
+    const { email, password, name } = req.body;
+
+    const client = new Client();
+    client
+        .setEndpoint('https://cloud.appwrite.io/v1') // Appwrite endpoint URL
+        .setProject('667eeaf9002912d4f3ab'); // Appwrite proje ID'si
+
+    const users = new Users(client);
+
+    try {
+        const response = await users.create('unique()', email, password, name);
+        res.json(response);
+    } catch (error) {
+        console.error('Error creating user:', error);
+      
+    }
+});
